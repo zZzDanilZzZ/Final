7182f26156992f8376f9a3b36546c39009bec0c1
Danil <zekeriyaev_d@auca.kg>
2025-12-15 11:11:27 +0600

    aaaa

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,18 +1,19 @@
+import React from 'react'
+import "./ContactItem.css"
 
-
-export default function ContactItem (contact) {
-
-
-    return (
-        <li className="contact-item">
-            <p className="contact-item__name">{contact.name}</p>
+export default function ContactItem({contact, onDelete}) {
+  return (
+    <li className='contact-item'>
+        <div className='contact-item__info'>
+            <p className='contact-item__name'>{contact.name}</p>
             <p>{contact.phone}</p>
             <p>{contact.email}</p>
+        </div>
 
-            <button className="contact-list__delete" onClick={onDelete}>
-                X   
-            </button>
-        </li>
-        
-    )
+        <button className='contact-item__delete' onClick={() => onDelete(contact.id)}>
+            Delete
+        </button>
+    </li>
+  )
 }
+
