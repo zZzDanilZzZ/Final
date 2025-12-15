b96e77f493311a5d7d696a7aab0ff417ee5288fd
Danil <zekeriyaev_d@auca.kg>
2025-12-15 11:12:02 +0600

    ContactList
    Fix typo in 'length' check and update rendering logic.

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,21 @@
-import ContactItem from "../ContactItem/ContactItem";
+import React from 'react'
+import ContactItem from '../ContactItem/ContactItem'
+
+export default function ContactList({contacts, onDelete}) {
+    if (contacts.lenght === 0) {
+        return <p className='contact-list__empty'>Список контактов пуст</p>
+    }
 
 
-export default function ContactList(contact, onDelete) {
     return (
-        <div className="contact-list">
-            <ContactItem contact={contact} />
-        </div>
-
+        <ul className='contact-list'> 
+            {contacts.map(contact => (
+                <ContactItem 
+                    key={contact.id}
+                    contact={contact}
+                    onDelete={onDelete}
+                />
+            ))}
+        </ul>
     )
 }
